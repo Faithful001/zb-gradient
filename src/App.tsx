5183c45732934968ad80a8289adacce5805aac9a
faithfultheking <83516086+Faithful001@users.noreply.github.com>
2025-08-06 23:49:29 +0100

    done with the mesh gradient animation and transition

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <div className="gradient-container w-full h-full flex items-center justify-center"></div>
+    </div>
+  );
+}
+
+export default App;
